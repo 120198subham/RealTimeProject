0ed4d2a9e4f4f1399ede135b851a5ee93f0e9a90
120198subham <120198subham@gmail.com>
2019-07-03 20:40:48 +0530

    Using Angular to Develop This Project
    Prediction Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { SearchComponent } from './search/search.component';
+import { SearchService } from './search.service';
+ 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
